da2bdc709059ad8fcc964baa7a9a76d58c9909dd
Dmitry <d.k.kulikov@gmail.com>
2018-04-10 08:08:03 +0300

    Add config to be able to request the PlayerEndpoint

--- src/services/MusicService.ts
+++ src/services/MusicService.ts
@@ -4,6 +4,9 @@ import { deserialize } from 'json-typescript-mapper';
 import { SearchRoot } from "../models/searchRoot";
 import { Track } from "../models/track";
 import { Entry } from "../models/entry";
+import * as config from "config";
+import axios from "axios";
+import * as FormData from "form-data";
 
 class MusicService {
     constructor() {
@@ -23,6 +26,14 @@ class MusicService {
         let theTrack: Track = new List<Entry>(deserializedObj.entries).Where(e => e.type == "1").Select(e => e.track).FirstOrDefault();
 
         let trackUrl: string = await gmp.play(theTrack.storeId);
+
+        let formData: FormData = new FormData();
+
+        formData.append("trackUrl", trackUrl);
+
+        let playerUrl: string = config.get("playerEndpointUrl");
+
+        axios.post(playerUrl, { trackUrl: trackUrl });
 
         return trackUrl;
     }
